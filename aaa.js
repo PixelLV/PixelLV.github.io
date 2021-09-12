61fb2da3ff516436c827b2541310d796496e5dc2
PixelLV <89537859+PixelLV@users.noreply.github.com>
2021-09-12 19:43:24 +0300

    Update aaa.js

--- aaa.js
+++ aaa.js
@@ -4,6 +4,9 @@ function Wheel(){
 function Wheel2(){
     window.location.replace("Wheel2.html");
 }
+function What(){
+    window.location.replace("What.html");
+}    
 function Home(){
     window.location.replace("index.html");
 }
